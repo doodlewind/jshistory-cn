b588e83b8a2275b1ab098b1f1fbee5b0e31f679b
doodlewind <doodlewind@users.noreply.github.com>
2020-09-29 13:01:09 +0000

    deploy: e2b60cce0f851cd8097029f4e00a5e9cb8472573

--- pagic.config.js
+++ pagic.config.js
@@ -1,13 +1,13 @@
-// 使用方式：安装 Deno v1.3.3, Pagic v0.9.1
+// 需安装 Deno 与 Pagic
 // curl -fsSL https://deno.land/x/install/install.sh | sh
 // deno install --unstable --allow-read --allow-write --allow-net --name=pagic https://deno.land/x/pagic/mod.ts
-// ~/.deno/bin/pagic build --serve --watch
+// pagic build --serve --watch
 export default {
     srcDir: './',
     theme: 'docs',
     plugins: ['sidebar', 'prev_next'],
     title: 'JavaScript 20 年',
-    description: '自 1995 年语言诞生到 2015 年 ES6 规范制定为止，共计 20 年的 JavaScript 语言演化经历。',
+    description: '自 1995 年语言诞生到 2015 年 ES6 规范制定为止，共计 20 年的 JavaScript 语言演化历程。',
     github: 'https://github.com/doodlewind/jshistory-cn',
     nav: [
     // { text: '阅读', link: '/' },
