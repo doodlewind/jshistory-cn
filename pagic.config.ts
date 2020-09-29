3bc435fd9464266e5ac1ea86fa991e45228c266c
Yifeng Wang <i@ewind.us>
2020-09-29 16:10:00 +0800

    build: update pagic config

--- pagic.config.ts
+++ pagic.config.ts
@@ -1,16 +1,14 @@
 // 使用方式：安装 Deno v1.3.3, Pagic v0.9.1
 // curl -fsSL https://deno.land/x/install/install.sh | sh
-// deno upgrade --version 1.3.3
 // deno install --unstable --allow-read --allow-write --allow-net --name=pagic https://deno.land/x/pagic/mod.ts
 // ~/.deno/bin/pagic build --serve --watch
 
 export default {
   srcDir: './',
-  exclude: [/\/demos\//, /\/public\//, /\/\./, /\/LICENSE/, /\/pagic\.config\.tsx/],
   theme: 'docs',
-  plugins: ['sidebar', 'prev_next', 'script'],
+  plugins: ['sidebar', 'prev_next'],
   title: 'JavaScript 20 年',
-  description: '由 JavaScript 之父 Brendan Eich 与 ES6 规范首席作者 Allen Wirfs-Brock 联合编写。',
+  description: '自 1995 年语言诞生到 2015 年 ES6 规范制定为止，共计 20 年的 JavaScript 语言演化经历。',
   github: 'https://github.com/doodlewind/jshistory-cn',
   nav: [
     // { text: '阅读', link: '/' },
